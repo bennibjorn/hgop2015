1f10d397854050c6ee66d90752009bd627a62bd1
Benjamin <bennibjorn@hotmail.com>
2015-12-02 21:24:09 +0000

    fixing syntax errors

--- dockerRestartServer.sh
+++ dockerRestartServer.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 docker kill ttt
-if [[ $rc != 0 ]]
-    then
-    #echo No container ttt to kill.
-fi
 docker rm ttt
 if [[ $rc != 0 ]]
     then
-    #echo Cannot remove container ttt.
+    #echo Cannot remove container ttt
     docker pull bennibjorn/tictactoe
     docker run -p 80:8080 -d -e "NODE_ENV=production" --name="ttt" bennibjorn/tictactoe
     exit $rc
