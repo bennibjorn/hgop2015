7846168712d038c6aa427547836935177fbe9f6d
Benjamin <bennibjorn@hotmail.com>
2015-12-12 16:34:46 +0000

    Less for frontend and makeMoveTests

--- server/model/tictactoe/tictactoeCommandHandler.js
+++ server/model/tictactoe/tictactoeCommandHandler.js
@@ -1,13 +1,54 @@
+'use strict';
+
 var _ = require('lodash');
 module.exports = function tictactoeCommandHandler(events) {
   var gameState = {
     gameCreatedEvent : events[0],
-    board: [['','',''],['','',''],['','','']]
+    board: [['','',''],['','',''],['','','']],
+  };
+
+  const winCondition = (currentTurn) => {
+      console.log("checking win condition");
+      // vertical/horizontal win check
+      let b = gameState.board;
+      for (let i = 0; i < 3; i++) {
+          let g = 0;
+          // horizontal
+          if (b[i][g] === currentTurn && b[i][g+1] === currentTurn && b[i][g+2] === currentTurn) {
+              return true;
+          }
+          // vertical
+          if (b[g][i] === currentTurn && b[g+1][i] === currentTurn && b[g+2][i] === currentTurn) {
+              console.log("checking board[" + g + "][" + b + "]");
+              console.log("vertical winning move");
+              return true;
+          }
+      }
+      // diagonal
+      if ((b[0][0] === currentTurn && b[1][1] === currentTurn && b[2][2] === currentTurn) ||
+           b[0][2] === currentTurn && b[1][1] === currentTurn && b[2][0] === currentTurn) {
+               return true;
+      } else {
+          console.log("no game winning move, returning false");
+          return false;
+      }
+
+  };
+
+  const drawCondition = () => {
+      for (var i = 0; i < 3; i++) {
+          for (var u = 0; u < 3; u++) {
+              if (gameState.board[i][u] === '') {
+                  return false;
+              }
+          }
+      }
+      return true;
   };
 
   var eventHandlers={
     'MoveMade': function(event){
-      gameState.board[event.x][event.y] = event.side;
+        gameState.board[event.x][event.y] = event.side;
     }
   };
 
@@ -62,16 +103,34 @@ module.exports = function tictactoeCommandHandler(events) {
           timeStamp: cmd.timeStamp
         }]
       }
-      return [{
-        id: cmd.id,
-        event: "MoveMade",
-        userName: cmd.userName,
-        name:gameState.gameCreatedEvent.name,
-        x:cmd.x,
-        y:cmd.y,
-        side:cmd.side,
-        timeStamp: cmd.timeStamp
+      var e = [{
+          id: cmd.id,
+          event: "MoveMade",
+          userName: cmd.userName,
+          name:gameState.gameCreatedEvent.name,
+          x:cmd.x,
+          y:cmd.y,
+          side:cmd.side,
+          timeStamp: cmd.timeStamp
       }]
+      if (winCondition(cmd.side)) {
+            e.push([{
+              id: cmd.id,
+              event: "GameWon",
+              userName: cmd.userName,
+              name:gameState.gameCreatedEvent.name,
+              side:cmd.side,
+              timeStamp: cmd.timeStamp
+            }])
+        } else if (drawCondition()) {
+          e.push([{
+              id: cmd.id,
+              event: "Draw",
+              name:gameState.gameCreatedEvent.name,
+              timeStamp: cmd.timeStamp
+          }])
+        }
+        return e;
     }
   };
 
--- server/model/tictactoe/whenMakeMove.spec.js
+++ server/model/tictactoe/whenMakeMove.spec.js
@@ -83,4 +83,55 @@ describe('when make move command', function(){
             }];
         });
     });
+    describe('on game ending move', function() {
+        it('should return game won on a vertical line', function() {
+            given.push({
+                id: "13375",
+                event: "MoveMade",
+                userName: "Benni",
+                name: "TicGame",
+                x: 0,
+                y: 0,
+                side: "X",
+                timeStamp: "2015.12.09T11:08:00"
+            });
+            given.push({
+                id: "13375",
+                event: "MoveMade",
+                userName: "Benni",
+                name: "TicGame",
+                x: 0,
+                y: 1,
+                side: "X",
+                timeStamp: "2015.12.09T11:08:01"
+            });
+            when = {
+                id: "13375",
+                comm: "MakeMove",
+                userName: "Benni",
+                x: 0,
+                y: 2,
+                side: "X",
+                timeStamp: "2015.12.09.T11:10:02"
+            };
+            then = [{
+                id: "13375",
+                event: "MoveMade",
+                userName: "Benni",
+                name: "TicGame",
+                x: 0,
+                y: 2,
+                side: "X",
+                timeStamp: "2015.12.09T11:08:02"
+            },
+            {
+                id: "13375",
+                event: "GameWon",
+                userName: "Benni",
+                name: "TicGame",
+                side: "X",
+                timeStamp: "2015.12.09.T11:10:02"
+            }];
+        });
+    });
 });
