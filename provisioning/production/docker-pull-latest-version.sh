653352c45118a77fb5f01a494d4559e46c925402
Benjamin <bennibjorn@hotmail.com>
2015-12-18 16:58:02 +0000

    The rest of the stuff I hadn't pushed yet

--- provisioning/production/docker-pull-latest-version.sh
+++ provisioning/production/docker-pull-latest-version.sh
@@ -2,5 +2,5 @@ echo Running revision $2 on port $1
 
 docker kill tictactoe$1
 docker rm tictactoe$1
-docker pull gulli/tictactoe:$2
-docker run -p $1:8080 -d --name tictactoe$1 -e "NODE_ENV=production" gulli/tictactoe:$2
+docker pull bennibjorn/tictactoe:$2
+docker run -p $1:8080 -d --name tictactoe$1 -e "NODE_ENV=production" bennibjorn/tictactoe:$2
